227908b0e0408f77f2ab360681c3cef54f5e3da0
Gaurav Raul <raulgaurav96@gmail.com>
2016-03-15 13:23:01 +0530

    Added boilerplate code and login anatomy.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Payroll Management System </title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <h1> <?php echo "Welcome To ACE Tech." ?> </h1>
+    <?php include("login.php"); ?> 
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+
+<form action="main.php" method="post">
+	Username:<input type="text" name="username"></input>
+	<br>
+	Password:<input type="password" name="password"></input>
+</form>
+
+</body>
+</html>
