e96700ac85a3f1cc61ecfafd62fb805e24ab0f1e
Gaurav Raul <raulgaurav96@gmail.com>
2016-03-17 23:03:38 +0530

    Successfully  made login from data base module.

--- main.php
+++ main.php
@@ -7,19 +7,41 @@
 <body>
 
      <?php 
-           $user = $_POST["username"];
-           $pass = $_POST["password"];
+     		if(isset($_POST['username'])){
+     			$user = $_POST["username"];
+     		}else{
+     			$user = '';
+     		}
+           
+           if(isset($_POST['password'])){
+     			 $pass = $_POST["password"];
+     		}else{
+     			$pass = '';
+     		} ?>
+          
+       <?php require("connection.php") ?>   
+       	<?php $sql_statement  = " SELECT username FROM admin WHERE username = '".$user."' AND password = '".$pass."' ";
+       			$result = mysql_query($sql_statement);
+       			if(!$result){
+	
+					$outputDisplay .= "<br><font color=red>MYSQL No: ".mysql_errno();
+					$outputDisplay .= "<br>MYSQL Error: ".mysql_error();
+					$outputDisplay .= "<br>SQL Statement: ".$sql_statement;
+					$outputDisplay .= "<br>MySQL Affected Rows: ".mysql_affected_rows()."</font><br>";
+				}else{
+					$numresults = mysql_num_rows($result);
 
-           if($user == "gaurav" && $pass == "gaurav@ace" || $user == "sagar" && $pass == "sagar@ace" || $user == "harsh" && $pass == "harsh@ace" ){?>
-           	 <h1> <?php echo " Welcome " . ucfirst($user); ?>  </h1>
-           <?php }
-           else{ echo "<script> alert(\"Invalid Username OR Password!!!\");window.location.href='index.php'</script>"; 
-       			}
-
-        ?>
+					if($numresults == 0){
+						 echo "<script> alert(\"Invalid Username OR Password!!!\");window.location.href='index.php'</script>"; 
+						}else{
+							 echo "<h1> Welcome " .ucfirst($user)."</h1>";
+						}
+					}
+       	?>
        
-        <?php require("selectFromEmployee.php"); ?>
-
+ 
+      
+        <?php require("selectFromEmployee.php");?>
 
       
 </body>
